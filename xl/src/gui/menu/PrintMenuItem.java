8ce0870467d09be1bbfb155ea101e9bdcc182e49
Jagge <ja0186an-s@student.lu.se>
2019-05-14 10:29:26 +0200

    Flyttat till model

--- xl/src/gui/menu/PrintMenuItem.java
+++ xl/src/gui/menu/PrintMenuItem.java
@@ -2,6 +2,8 @@ package gui.menu;
 
 import gui.StatusLabel;
 import gui.XL;
+import model.XLException;
+
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.PrintJob;
@@ -11,7 +13,6 @@ import java.awt.event.ActionListener;
 import java.awt.print.PrinterException;
 import java.awt.print.PrinterJob;
 import javax.swing.JMenuItem;
-import util.XLException;
 
 class PrintMenuItem extends JMenuItem implements ActionListener {
     private StatusLabel statusLabel;
