b6c40d2e8f2819b3158e3c0ddc14fa1138aab72e
Gustav <nzxtify@gmail.com>
2019-05-16 12:23:10 +0200

    efter mötet

---
+++ xl/src/model/Circular.java
@@ -0,0 +1,5 @@
+package model;
+
+public class Circular implements Slots{
+
+}
---
+++ xl/src/model/XLSheet.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.util.HashMap;
+
+public class XLSheet {
+	
+	private HashMap<String, Slots> sheet = new HashMap<String, Slots>();
+	
+	
+	public XLSheet() {
+		
+	}
+
+}
