c8b76b9afc402a47a525aa8d65df7643a181890c
Jagge <ja0186an-s@student.lu.se>
2019-05-20 11:16:24 +0200

    Observables

--- xl/src/gui/Editor.java
+++ xl/src/gui/Editor.java
@@ -1,10 +1,28 @@
 package gui;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Observable;
+import java.util.Observer;
+
 import javax.swing.JTextField;
 
-public class Editor extends JTextField {
+public class Editor extends JTextField implements ActionListener, Observer {
     public Editor() {
         setBackground(Color.WHITE);
+        addActionListener(this);
     }
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
 }
