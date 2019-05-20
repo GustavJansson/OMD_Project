d98f88c0e342f8c30150bd30b395ac36dd71daf6
Jagge <ja0186an-s@student.lu.se>
2019-05-20 11:40:35 +0200

    CurrentSlot

--- xl/src/gui/CurrentLabel.java
+++ xl/src/gui/CurrentLabel.java
@@ -1,24 +1,17 @@
 package gui;
 
 import java.awt.Color;
+import java.util.Observer;
 
-import javafx.beans.InvalidationListener;
-import javafx.beans.Observable;
-
-public class CurrentLabel extends ColoredLabel implements Observable{
+public class CurrentLabel extends ColoredLabel implements Observer{
     public CurrentLabel() {
         super("A1", Color.WHITE);
     }
 
 	@Override
-	public void addListener(InvalidationListener arg0) {
+	public void update(java.util.Observable o, Object arg) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
-	public void removeListener(InvalidationListener arg0) {
-		// TODO Auto-generated method stub
-		
-	}
 }
---
+++ xl/src/gui/CurrentSlot.java
@@ -0,0 +1,24 @@
+package gui;
+
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+
+public class CurrentSlot implements Observable {
+	
+	public CurrentSlot() {
+		
+	}
+	
+	@Override
+	public void addListener(InvalidationListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeListener(InvalidationListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
