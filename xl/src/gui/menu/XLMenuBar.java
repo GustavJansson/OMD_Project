250e1b99c6b389af6660c9a4c444b1142b95485b
Jagge <ja0186an-s@student.lu.se>
2019-05-20 16:21:10 +0200

    HEha

--- xl/src/gui/menu/XLMenuBar.java
+++ xl/src/gui/menu/XLMenuBar.java
@@ -1,13 +1,16 @@
 package gui.menu;
 
+import gui.CurrentSlot;
 import gui.StatusLabel;
 import gui.XL;
 import gui.XLList;
+import model.XLSheet;
+
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 
 public class XLMenuBar extends JMenuBar {
-    public XLMenuBar(XL xl, XLList xlList, StatusLabel statusLabel) {
+    public XLMenuBar(XL xl, XLList xlList, StatusLabel statusLabel, XLSheet sheet, CurrentSlot currentSlot) {
         JMenu file = new JMenu("File");
         JMenu edit = new JMenu("Edit");
         file.add(new PrintMenuItem(xl, statusLabel));
@@ -15,8 +18,8 @@ public class XLMenuBar extends JMenuBar {
         file.add(new LoadMenuItem(xl, statusLabel));
         file.add(new NewMenuItem(xl));
         file.add(new CloseMenuItem(xl, xlList));
-        edit.add(new ClearMenuItem());
-        edit.add(new ClearAllMenuItem());
+        edit.add(new ClearMenuItem(sheet, currentSlot));
+        edit.add(new ClearAllMenuItem(sheet));
         add(file);
         add(edit);
         add(new WindowMenu(xlList));
