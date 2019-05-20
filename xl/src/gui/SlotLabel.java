253563c3dbabd7ec12d36145dd2c942264e5146e
Gustav <nzxtify@gmail.com>
2019-05-20 11:28:22 +0200

    slot

--- xl/src/gui/SlotLabel.java
+++ xl/src/gui/SlotLabel.java
@@ -1,9 +1,18 @@
 package gui;
 
 import java.awt.Color;
+import java.util.Observable;
+import java.util.Observer;
 
-public class SlotLabel extends ColoredLabel {
+public class SlotLabel extends ColoredLabel implements Observer{
     public SlotLabel() {
         super("                    ", Color.WHITE, RIGHT);
     }
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+		this.setText((String)arg);
+	}
 }
