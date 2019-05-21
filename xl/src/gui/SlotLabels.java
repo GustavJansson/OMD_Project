1f88ad0e00b17f17fe63137acc945fcca96aa353
Gustav <nzxtify@gmail.com>
2019-05-21 12:37:12 +0200

    En fix så att endast currentLabel är gul

--- xl/src/gui/SlotLabels.java
+++ xl/src/gui/SlotLabels.java
@@ -27,6 +27,6 @@ public class SlotLabels extends GridPanel {
         }
         SlotLabel firstLabel = labelList.get(0);
         firstLabel.setBackground(Color.YELLOW);
-        firstLabel.mousePressed(null);
+        slot.set(firstLabel);
     }
 }
