0cf5f89fb2c29213962c479d595afbde9b2969f8
Gustav <nzxtify@gmail.com>
2019-05-27 09:39:31 +0200

    exception i slotcreator

--- xl/src/model/SlotCreator.java
+++ xl/src/model/SlotCreator.java
@@ -1,20 +1,28 @@
 package model;
 
+import java.io.IOException;
+
 import expr.ExprParser;
 
 public class SlotCreator {
 
-	public static Slot toSlot(String s) {
+	public static Slot toSlot(String s) throws IOException {
+		
+		Expression expr;
+		
 		if (s.length() > 0 && s.charAt(0) == '#') {
 			return new Comment(s.substring(1));
 		}
 		else {
 			ExprParser parser = new ExprParser();
 			try {
-				return new Expression(parser.build(s));
-			} catch (Exception e) {
-				return null;
+				expr = new Expression(parser.build(s));
+				
+			} catch (XLException e) {
+				throw e;
 			}
+			
+			return expr;
 		}
 	}
 	
